5793fb45ac78d402c9a425ca384e2b82525f45ee
Benjamin Martin <the.one.martin@gmail.com>
2024-10-22 00:08:25 -0400

    Add operators

--- src/presentations/module-01-variables/Lesson03-operators.tsx
+++ src/presentations/module-01-variables/Lesson03-operators.tsx
@@ -27,33 +27,40 @@ a %= b; // Set a to the remainder of a / b;`;
 
 
   const comparison = `let bool = 6 < 10; // Less Than
-bool = 6 <= 10; // Less than or Equal
-bool = 10 > 6; // Greater than
-bool = 10 >= 6; // Greater than or Equal
-bool = 10 == "10"; // Equal (Loose)
-bool = 10 === 10; // Equal (Strict)
-bool = 10 != "11"; // Not Equal (Loose)
-bool = 10 !== "10"; // Not Equal (Strict)
-bool = true ? "true" : "false"; // Ternary`;
+bool = 6 <= 10;     // Less than or Equal
+bool = 10 > 6;      // Greater than
+bool = 10 >= 6;     // Greater than or Equal
+bool = 10 == "10";  // Equal (Loose)
+bool = 10 === 10;   // Equal (Strict)
+bool = 10 != "11";  // Not Equal (Loose)
+bool = 10 !== "10"; // Not Equal (Strict)`;
 
   const logical = `let bool = true && true; // AND
 bool = true || false; // OR
 bool = !false; // NOT`
 
+  const ternary = `let bool = true ? "true" : "false"; // Ternary`;
+
   /**\
-   * , comma
-   * + unary plus
-   * ! not
    * ( ) grouping
+   * , comma
    * typeof
    * instanceof
-   * ?? nullish coalescing
-   * ??= 
    * ... spread
    */
 
-  const otherUnary = ``;
+  const castingToNumber = `let value = +"30"; // Convert to 30
+value = +true // 1
+value = +false // 0
+value = +null // 0
+value = +[] // 0
+value = + (a) => a; // NaN`;
 
+
+  const nullish = `let something = null ?? "default";
+let car = {color: "blue"};
+car.speed ??= 10; // set to 10
+car.color ??= "red"; // stays as "blue"`;
 
   const truthy = `// Truthy
 let truthyValues = [1, "1", "0", 1n, [], {}, Infinity, 0b1, 0x1];
@@ -61,35 +68,61 @@ let truthyValues = [1, "1", "0", 1n, [], {}, Infinity, 0b1, 0x1];
 // Falsy
 let falsyValues = [0, "", 0n, null, undefined, 0b0, 0x0];`
 
+  const doubleNot = `let val = !! "word"; // true
+val = !! 100; // true
+val = !! 0; // false
+val = !! ""; // false
+`
+
   return (
     <div className="reveal" ref={deckDivRef}>
       <div className="slides">
+
+
         <section>
           <h1>Operators</h1>
         </section>
 
 
-        <section>
+
+        <section data-auto-animate>
           <h2>Mathematical</h2>
-          <CodeBlock code={mathematical}></CodeBlock>
+          <CodeBlock id="code-box" code={mathematical}></CodeBlock>
         </section>
 
 
-        <section>
+
+        <section data-auto-animate>
           <h2>Assignment</h2>
-          <CodeBlock code={assignment}></CodeBlock>
+          <CodeBlock id="code-box" code={assignment}></CodeBlock>
         </section>
 
 
-        <section>
+
+        <section data-auto-animate>
           <h2>comparison</h2>
-          <CodeBlock code={comparison}></CodeBlock>
+          <CodeBlock id="code-box" code={comparison}></CodeBlock>
         </section>
 
 
-        <section>
+
+        <section data-auto-animate>
           <h2>logical</h2>
-          <CodeBlock code={logical}></CodeBlock>
+          <CodeBlock id="code-box" code={logical}></CodeBlock>
+        </section>
+
+
+
+        <section>
+          <h3>Nullish</h3>
+          <CodeBlock id="code-box" lineNumbers code={nullish} />
+        </section>
+
+
+
+        <section>
+          <h3>Changing Type</h3>
+          <CodeBlock id="code-box" lineNumbers code={castingToNumber} />
         </section>
 
 
@@ -102,6 +135,20 @@ let falsyValues = [0, "", 0n, null, undefined, 0b0, 0x0];`
             Basically, any non-empty, non-zero value will evaluate to truthy. Everything else will evaluate to falsy.<br /><br />
             - "0" is a unique case. Although it is 0, it is a string with a value, so it is truthy.
           </Notes>
+        </section>
+
+
+
+        <section>
+          <h3>Double Not</h3>
+          <CodeBlock id="code-box" lineNumbers code={doubleNot} />
+        </section>
+
+
+
+        <section data-auto-animate>
+          <h3>Ternary</h3>
+          <CodeBlock id="code-box" lineNumbers code={ternary} />
         </section>
 
 
